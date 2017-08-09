d63dd64851e1dd82f8f1fe75f2a5ca0f832a7c95
mpgn <martial.puygrenier@gmail.com>
2017-08-10 00:49:20 +0200

    First draft of the dashboard
    - backend ok
    - front ok
    
    TODO: update dashboard

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # HallOfFame
-Hall Of Fame dashboard
+
+Hall Of Fame dashboard  for Root-me.org. All users are manually added to the file `users.json`, then with a cron job, the script `update.py` do the rest.
+
+![screenshot](https://i.gyazo.com/1823b9244ef0c4c98d5877f837880dda.png)
+
+## Informations
+
+Since root-me doesn't have any API, this is dirty parsing ! But it's working ;)
+
+### Add/Update a user
+
+Add the user in the `users.json` like this :
+```
+python update.py 'username' 'realname'
+```
+
+Update the json like this :
+```
+python3 update.py
+```
+
+Use a cron job to update the script every night ;)
+
+## History
+
+* 0.0.1
+	* First release
+
+## Credits
+
+* [@mpgn_x64](https://twitter.com/mpgn_x64)
+
+## License
+
+MIT License
---
+++ updater/update.py
@@ -0,0 +1,93 @@
+import json
+import re
+import requests
+import sys
+
+with open('users.json', 'r') as data_file:
+	data = json.load(data_file)
+
+def wrong_arg():
+	print("[-] Missing or wrong argument")
+	print("[-] Example: \n\tupdate.py update -> to update the json\n\tupdate.py add username realname -> to add a user to the json")
+	sys.exit()
+
+def update_users(user):
+	print("[+] Starting update profil")
+	r = requests.get('https://www.root-me.org/'+ user['username'] + '?inc=score')
+	regex = r'<span.*?>\n(.*)<span.*?>/(.*)</span>'
+	matches = re.search(regex, r.text)
+	if matches:
+		user['rank'] = "{group}".format(group = matches.group(1))
+		total_rank = "{group}".format(group = matches.group(2))
+
+	regex = r'<span.*?>\n(.*)&nbsp;Points'
+	matches = re.search(regex, r.text)
+	if matches:
+		user['points'] = "{group}".format(group = matches.group(1))
+
+	regex = r'<span.*?>\n([a-z]*)&nbsp;'
+	matches = re.search(regex, r.text)
+	if matches:
+		user['status'] = "{group}".format(group = matches.group(1))
+
+	regex = r'<span.*?>\n([0-9]*)/([0-9]*)'
+	matches = re.search(regex, r.text)
+	if matches:
+		user['challenges'] = "{group}".format(group = matches.group(1))
+		total_challenge = "{group}".format(group = matches.group(2))
+
+	regex = r'<h1 itemprop="givenName"><img class=\'.*? logo_auteur .*?\' src="(.*?)"'
+	matches = re.search(regex, r.text)
+	if matches:
+		avatar = "{group}".format(group = matches.group(1))
+		user['avatar'] = "https://www.root-me.org/" + avatar
+
+	print("[+]", user['username'], user['realn'], user['avatar'], user['rank'], user['points'] ,user['challenges'], user['status'])
+
+	with open('users.json', 'w') as data_file:
+		json.dump(data, data_file)
+
+	print("[+] End update profil")
+
+def update():
+	print("[+] Starting Update generic information")
+
+	r = requests.get('https://www.root-me.org/fr/Communaute/Classement/')
+	regex = r'<a href=".*?>([0-9]+)</a>'
+	matches = re.search(regex, r.text)
+	if matches:
+		data['total_points'] = "{group}".format(group = matches.group(1))
+
+	r = requests.get('https://www.root-me.org/Capitaine-John?inc=score')
+	regex = r'<span.*?>\n([0-9]*)/([0-9]*)'
+	matches = re.search(regex, r.text)
+	if matches:
+		data['total_challenge'] = "{group}".format(group = matches.group(2))
+
+	print("[+] End of update generic information")
+	for user in data['users']:
+		update_users(user)
+
+def add_user(username, realn):
+	print("[+] Starting to add user", username)
+
+	data['users'].append({"username": username , "realn": realn, "avatar": "https://www.root-me.org/local/cache-vignettes/L48xH48/auton0-5220c.png", "rank": "0", "points": "0", "challenges": "0", "status": "newbie"})
+	with open('users.json', 'w') as data_file:
+		json.dump(data, data_file)
+
+	print("[+] End of add")
+	# then update lasted
+	update_users(data['users'][-1])
+
+
+### STARTING POINT####
+if len(sys.argv) < 2:
+	wrong_arg()
+else:
+	if sys.argv[1] == "update":
+		update()
+	elif sys.argv[1] == "add" and len(sys.argv) == 4:
+		add_user(sys.argv[2],sys.argv[3])
+	else:
+		wrong_arg()
+
