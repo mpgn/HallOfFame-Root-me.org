91184506b9f0a703991fac164f935e8cde6b5da2
mpgn <martial.puygrenier@gmail.com>
2017-08-11 00:27:29 +0200

    Add challenges statistics form a user
    :)

--- updater/update.py
+++ updater/update.py
@@ -3,7 +3,7 @@ import re
 import requests
 import sys
 
-with open('users.json', 'r') as data_file:
+with open('../site/users.json', 'r') as data_file:
 	data = json.load(data_file)
 
 def wrong_arg():
@@ -14,6 +14,12 @@ def wrong_arg():
 def update_users(user):
 	print("[+] Starting update profil")
 	r = requests.get('https://www.root-me.org/'+ user['username'] + '?inc=score')
+
+	regex = r'<span class=" forum" >(.*)</span>'
+	matches = re.search(regex, r.text)
+	if matches:
+		user['username_r'] = "{group}".format(group = matches.group(1))
+
 	regex = r'<span.*?>\n(.*)<span.*?>/(.*)</span>'
 	matches = re.search(regex, r.text)
 	if matches:
@@ -42,9 +48,16 @@ def update_users(user):
 		avatar = "{group}".format(group = matches.group(1))
 		user['avatar'] = "https://www.root-me.org/" + avatar
 
-	print("[+]", user['username'], user['realn'], user['avatar'], user['rank'], user['points'] ,user['challenges'], user['status'])
+	print("[+]", user['username'], user['username_r'], user['realn'], user['avatar'], user['rank'], user['points'] ,user['challenges'], user['status'])
 
-	with open('users.json', 'w') as data_file:
+	# Get all data from challenge
+	regex = r'<span.*?>\n([0-9]*)&nbsp;Points&nbsp;([0-9]*)/([0-9]*)'
+	matches = re.finditer(regex, r.text)
+	for matchNum, match in enumerate(matches):
+		matchNum = matchNum + 1
+		print("{group}".format(group = match.group(1)),"=>", "{group}".format(group = match.group(2)),'/', "{group}".format(group = match.group(3)))
+
+	with open('../site/users.json', 'w') as data_file:
 		json.dump(data, data_file)
 
 	print("[+] End update profil")
@@ -52,6 +65,8 @@ def update_users(user):
 def update():
 	print("[+] Starting Update generic information")
 
+	# get generic data
+	global r 
 	r = requests.get('https://www.root-me.org/fr/Communaute/Classement/')
 	regex = r'<a href=".*?>([0-9]+)</a>'
 	matches = re.search(regex, r.text)
@@ -71,16 +86,15 @@ def update():
 def add_user(username, realn):
 	print("[+] Starting to add user", username)
 
-	data['users'].append({"username": username , "realn": realn, "avatar": "https://www.root-me.org/local/cache-vignettes/L48xH48/auton0-5220c.png", "rank": "0", "points": "0", "challenges": "0", "status": "newbie"})
-	with open('users.json', 'w') as data_file:
+	data['users'].append({"username": username, "username_r": "", "realn": realn, "avatar": "https://www.root-me.org/local/cache-vignettes/L48xH48/auton0-5220c.png", "rank": 0, "points": 0, "challenges": 0, "status": "newbie"})
+	with open('../site/users.json', 'w') as data_file:
 		json.dump(data, data_file)
 
 	print("[+] End of add")
 	# then update lasted
 	update_users(data['users'][-1])
 
-
-### STARTING POINT####
+# ## STARTING POINT####
 if len(sys.argv) < 2:
 	wrong_arg()
 else:
